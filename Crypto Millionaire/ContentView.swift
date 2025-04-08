e6acf2662ea91c4d72326188bc364e546c3ee8cb
Hubert <h.wojtowicz44@gmail.com>
2025-04-08 19:46:49 +0200

    Initial Commit

---
+++ Crypto Millionaire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Millionaire
+//
+//  Created by Hubert Wojtowicz on 08/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Millionaire/Crypto_MillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_MillionaireApp.swift
+//  Crypto Millionaire
+//
+//  Created by Hubert Wojtowicz on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_MillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
