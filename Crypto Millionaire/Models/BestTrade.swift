d4c70eebbbfa3160586787d926c2c9ee7b3d8ef5
Hubert <h.wojtowicz44@gmail.com>
2025-04-09 17:25:14 +0200

    add BestTrade structure

--- Crypto Millionaire/Models/BestTrade.swift
+++ Crypto Millionaire/Models/BestTrade.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct BestTrade: Identifiable, Codable {
+    let id: String
+    let cryptoId: String
+    let cryptoSymbol: String
+    let cryptoName: String
+    let buyPrice: Double
+    let sellPrice: Double
+    let quantity: Double
+    let profit: Double
+    let profitPercentage: Double
+    let buyTimestamp: Date
+    let sellTimestamp: Date
+    
+    var formattedProfit: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        return formatter.string(from: NSNumber(value: profit)) ?? "$0.00"
+    }
+    
+    var formattedProfitPercentage: String {
+        return String(format: "%.2f%%", profitPercentage)
+    }
+}
