7f4ebfce8853574d8777f5008f785d1ea51a721b
Hubert <h.wojtowicz44@gmail.com>
2025-04-09 17:14:17 +0200

    add UserBalance structure

--- Crypto Millionaire/Models/UserBalance.swift
+++ Crypto Millionaire/Models/UserBalance.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct UserBalance: Codable {
+    var balance: Double
+    var initialBalance: Double
+    var timestamp: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case balance, initialBalance, timestamp
+    }
+}
+
+extension UserBalance {
+    static func defaultbalance() -> UserBalance {
+        return UserBalance(balance: 10000.0, initialBalance: 10000.0, timestamp: Date())
+    }
+    
+    mutating func reset() {
+        self.balance = self.initialBalance
+        self.timestamp = Date()
+    }
+    mutating func update(amount: Double) {
+        self.balance += amount
+    }
+}
+
