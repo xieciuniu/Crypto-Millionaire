c11a1b7c1ce38dfacb8b3eb1287153d3e58c281a
Hubert <h.wojtowicz44@gmail.com>
2025-04-09 16:55:05 +0200

    add Portfolio structure

--- Crypto Millionaire/Models/Portfolio.swift
+++ Crypto Millionaire/Models/Portfolio.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+struct Portfolio: Codable, Identifiable {
+    let id: String
+    let cryptoId: String
+    let cryptoSymbol: String
+    let cryptoName: String
+    var quantity: Double
+    var averageBuyPrice: Double
+    
+    var currentValue: Double = 0.0
+    var profitLoss: Double = 0.0
+    var profitLossPercentage: Double = 0.0
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case cryptoId
+        case cryptoSymbol
+        case cryptoName
+        case quantity
+        case averageBuyPrice
+    }
+}
+
+
+extension Portfolio {
+    mutating func updateCurrentValue(withPrice price: Double) {
+        currentValue = quantity * price
+        profitLoss = currentValue - (averageBuyPrice * quantity)
+        profitLossPercentage = (price - averageBuyPrice) / averageBuyPrice * 100
+    }
+    
+    mutating func add(quantity: Double, atPrice price: Double) {
+        let newTotalCost = (self.quantity * self.averageBuyPrice) + (quantity * price)
+        let newTotalQuantity = self.quantity + quantity
+        self.averageBuyPrice = newTotalCost / newTotalQuantity
+        self.quantity = newTotalQuantity
+    }
+    
+    mutating func substract(quantity: Double) {
+        self.quantity -= quantity
+    }
+}
