63cc48159827378d6aeb72cdfa2b3f7e3cb9fc44
Hubert <h.wojtowicz44@gmail.com>
2025-04-09 16:44:00 +0200

    add Transaction structure

--- Crypto Millionaire/Models/Transaction.swift
+++ Crypto Millionaire/Models/Transaction.swift
@@ -7,4 +7,52 @@
 
 import Foundation
 
+struct Transaction: Identifiable, Codable {
+    let id: String
+    let cryptoID: String
+    let cryptoSymbol: String
+    let cryptoName: String
+    let type: TransactionType
+    let amount: Double
+    let price: Double
+    let quantity: Double
+    let fee: Double
+    let timestamp: Date
+    
+    enum TransactionType: String, Codable {
+        case buy
+        case sell
+    }
+    
+    var totalValue: Double {
+        return quantity * price
+    }
+    
+    var feeAmount: Double {
+        return totalValue * fee
+    }
+    
+    var netAmount: Double {
+        switch type {
+        case .buy:
+            return -(totalValue + feeAmount)
+        case .sell:
+            return totalValue - feeAmount
+        }
+    }
+}
 
+extension Transaction {
+    init(crytpoID: String, cryptoSymbol: String, cryptoName: String, type: TransactionType, price: Double, quntity: Double, fee: Double = 0.001) {
+        self.id = UUID().uuidString
+        self.cryptoID = crytpoID
+        self.cryptoSymbol = cryptoSymbol
+        self.cryptoName = cryptoName
+        self.type = type
+        self.amount = price * quntity
+        self.price = price
+        self.quantity = quntity
+        self.fee = fee
+        self.timestamp = Date()
+    }
+}
